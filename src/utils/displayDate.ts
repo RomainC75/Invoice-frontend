d091f382b580ecea9ae97108e85ddd2ba003ac0e
RomainC75 <rom.chenard@gmail.com>
2022-10-31 13:30:42 +0100

    displayDate + try/catch

--- src/utils/displayDate.ts
+++ src/utils/displayDate.ts
@@ -1,10 +1,14 @@
 const months = ["Jan","Feb","Mars","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
 
 export const displayDate = (date:string):string =>{
-    const monthNumber = date.match(/(\d{4})-(\d{2})-(\d{2})/)
+    const monthNumber:RegExpMatchArray|null = date.match(/(\d{4})-(\d{2})-(\d{2})/)
     if(monthNumber && monthNumber.length>=4){
-        const monthName = months[parseInt(monthNumber[2])-1]
-        return `${monthNumber[3]} ${monthName} ${monthNumber[1]}`
+        try{
+            const monthName:string = months[parseInt(monthNumber[2])-1]
+            return `${monthNumber[3]} ${monthName} ${monthNumber[1]}`
+        }catch(e){
+            return ""
+        }
     }
     return ""
 }
