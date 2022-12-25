a9f2fd48e0111eccdbd96cd37f9b4cdb7055b90e
RomainC75 <rom.chenard@gmail.com>
2022-12-25 14:23:47 +0100

    InvoiceListElement responsive

--- src/components/InvoiceListElement.tsx
+++ src/components/InvoiceListElement.tsx
@@ -15,7 +15,7 @@ const InvoiceListElement = ({invoice}:InvoiceListElementInterface):JSX.Element =
     const {theme} = useSelector((state:StoresInterface)=>state.auth)
   return (
     <Link to={`/invoice/${invoice.id}`}>
-        <li className={theme ? "InvoiceListElement colorBgWhite" : "InvoiceListElement colorBg3"}> 
+        <li className={theme ? "InvoiceListElement colorBgWhite wide" : "InvoiceListElement colorBg3 wide"}> 
             <div className="sub">
               <div className={theme ? "id black" : "id white"}><span className="sharp color7">#</span>{invoice.id}</div>
               <p className={theme ? "dueDate color7": "dueDate color5"}>Due {invoice.paymentDue}</p>
@@ -24,6 +24,24 @@ const InvoiceListElement = ({invoice}:InvoiceListElementInterface):JSX.Element =
             </div>    
             <StatusInfo status={invoice.status}/>
             <GoChevronRight className="chevron color1"/>
+        </li>
+
+        <li className={theme ? "InvoiceListElement colorBgWhite smartphone" : "InvoiceListElement colorBg3 smartphone"}> 
+            <div className="sub">
+              <div className="sub__top">
+                <div className={theme ? "id black" : "id white"}><span className="sharp color7">#</span>{invoice.id}</div>
+                <div className={theme ? "name color7" : "name white"}>{invoice.clientName}</div>
+              </div>
+              <div className="sub__bottom">
+                <div className="left">
+                  <p className={theme ? "dueDate color7": "dueDate color5"}>Due {invoice.paymentDue}</p>
+                  <p className={theme ? "total black" : "total white"}>€ 1800.43</p>
+                </div>
+                <StatusInfo status={invoice.status}/>
+
+              </div>
+            </div>    
+            
         </li>
     </Link>
     
--- src/components/ItemsList.tsx
+++ src/components/ItemsList.tsx
@@ -1,68 +1,88 @@
-import React,{useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { ItemInterface } from '../@types/invoice'
 import { useSelector } from 'react-redux'
 import { StoresInterface } from '../@types/store'
 
 import './styles/itemsList.css'
 
-interface ItemsListInterface{
-    items:ItemInterface[] 
+interface ItemsListInterface {
+   items: ItemInterface[]
 }
 
-const ItemsList = ({items}:ItemsListInterface):JSX.Element => {
-    const {theme} = useSelector((state:StoresInterface)=>state.auth)
-    // const [innerWidthState, setInnerWidthState] = useState<null| number>(null)
+const ItemsList = ({ items }: ItemsListInterface): JSX.Element => {
+   const { theme } = useSelector((state: StoresInterface) => state.auth)
+   // const [innerWidthState, setInnerWidthState] = useState<null| number>(null)
 
-    // window.addEventListener('resize',()=>{
-    //     setInnerWidthState(window.innerWidth)
-    // })
+   // window.addEventListener('resize',()=>{
+   //     setInnerWidthState(window.innerWidth)
+   // })
 
-    
-  return (
-    <div className="ItemsList">
-
-        <div className={theme ? "details wide colorBg13" : "details wide colorBg4"}>
+   return (
+      <div className="ItemsList">
+         <div className={theme ? 'details wide colorBg13' : 'details wide colorBg4'}>
             <div className="name column">
-                <div className={theme ? "title p2 color7" : "title p2 color5"}>Item Name</div>
-                {items.map((item,i)=><div key={`$item.name}-${i}`} className={theme ? "p1 black":"p1 white"}>{item.name}</div>)}
+               <div className={theme ? 'title p2 color7' : 'title p2 color5'}>Item Name</div>
+               {items.map((item, i) => (
+                  <div key={`$item.name}-${i}`} className={theme ? 'p1 black' : 'p1 white'}>
+                     {item.name}
+                  </div>
+               ))}
             </div>
             <div className="quantity column">
-                <div className={theme ? "title p2 color7" : "title p2 color5"}>Qty.</div>
-                {items.map((item,i)=><div key={`$item.name}-${i}`} className={theme ? "p1 color7":"p1 color5"}>{item.quantity}</div>)}
+               <div className={theme ? 'title p2 color7' : 'title p2 color5'}>Qty.</div>
+               {items.map((item, i) => (
+                  <div key={`$item.name}-${i}`} className={theme ? 'p1 color7' : 'p1 color5'}>
+                     {item.quantity}
+                  </div>
+               ))}
             </div>
             <div className="price column">
-                <div className={theme ? "title p2 color7" : "title p2 color5"}>Price</div>
-                {items.map((item,i)=><div key={`$item.name}-${i}`} className={theme ? "p1 color7":"p1 color5"}>€ {item.price.toFixed(2)}</div>)}
+               <div className={theme ? 'title p2 color7' : 'title p2 color5'}>Price</div>
+               {items.map((item, i) => (
+                  <div key={`$item.name}-${i}`} className={theme ? 'p1 color7' : 'p1 color5'}>
+                     € {item.price.toFixed(2)}
+                  </div>
+               ))}
             </div>
             <div className="subTotal column">
-                <div className={theme ? "title p2 color7" : "title p2 color5"}>Total</div>
-                {items.map((item,i)=><div key={`$item.name}-${i}`} className={theme ? "p1 black":"p1 white"}>€ {(item.price*item.quantity).toFixed(2)}</div>)}
+               <div className={theme ? 'title p2 color7' : 'title p2 color5'}>Total</div>
+               {items.map((item, i) => (
+                  <div key={`$item.name}-${i}`} className={theme ? 'p1 black' : 'p1 white'}>
+                     € {(item.price * item.quantity).toFixed(2)}
+                  </div>
+               ))}
             </div>
-        </div>
-        {/* smartphone */}
-        <div className={theme ? "details smartphone colorBg13" : "details smartphone colorBg4"}>
-            {items.map(item=>
-                <div className="itemsLine">
-                    <div className="left">
-                        <p className={theme ? "p1 black":"p1 white"}>{item.name}</p>
-                        <p className={theme ? "p1 color7":"p1 color5"}>{item.quantity} x € {item.price.toFixed(2)}</p>
-                    </div>
-                    <div className="right">
-                        <p className={theme ? "p1 black":"p1 white"}>€ {(item.price*item.quantity).toFixed(2)}</p>
-                    </div>
-                </div>    
-            )}
-            
-        </div>
-        {/* sum */}
-        <div className={theme ? "total colorBg14" : "total colorBg8"}>
+         </div>
+         {/* smartphone */}
+         <div className={theme ? 'details smartphone colorBg13' : 'details smartphone colorBg4'}>
+            {items.map((item) => (
+               <div className="itemsLine">
+                  <div className="left">
+                     <p className={theme ? 'p1 black' : 'p1 white'}>{item.name}</p>
+                     <p className={theme ? 'p1 color7' : 'p1 color5'}>
+                        {item.quantity} x € {item.price.toFixed(2)}
+                     </p>
+                  </div>
+                  <div className="right">
+                     <p className={theme ? 'p1 black' : 'p1 white'}>€ {(item.price * item.quantity).toFixed(2)}</p>
+                  </div>
+               </div>
+            ))}
+         </div>
+         {/* sum */}
+         <div className={theme ? 'total colorBg14' : 'total colorBg8'}>
             <p className="p2">Amount Due</p>
-            <p className="totalNumber">€ {items.reduce((accu,current)=>{
-            return accu+current.price*current.quantity
-            },0).toFixed(2)}</p>
-        </div>
-    </div>
-  )
+            <p className="totalNumber">
+               €{' '}
+               {items
+                  .reduce((accu, current) => {
+                     return accu + current.price * current.quantity
+                  }, 0)
+                  .toFixed(2)}
+            </p>
+         </div>
+      </div>
+   )
 }
 
 export default ItemsList
