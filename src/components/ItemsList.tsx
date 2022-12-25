91f50db0a25ff0540c7b02c954fdf73ae80efca3
RomainC75 <rom.chenard@gmail.com>
2022-12-25 13:34:50 +0100

    ItemsList responsive

--- src/components/ItemsList.tsx
+++ src/components/ItemsList.tsx
@@ -6,15 +6,22 @@ import { StoresInterface } from '../@types/store'
 import './styles/itemsList.css'
 
 interface ItemsListInterface{
-    items:ItemInterface[]
+    items:ItemInterface[] 
 }
 
 const ItemsList = ({items}:ItemsListInterface):JSX.Element => {
     const {theme} = useSelector((state:StoresInterface)=>state.auth)
+    // const [innerWidthState, setInnerWidthState] = useState<null| number>(null)
 
+    // window.addEventListener('resize',()=>{
+    //     setInnerWidthState(window.innerWidth)
+    // })
+
+    
   return (
     <div className="ItemsList">
-        <div className={theme ? "details colorBg13" : "details colorBg4"}>
+
+        <div className={theme ? "details wide colorBg13" : "details wide colorBg4"}>
             <div className="name column">
                 <div className={theme ? "title p2 color7" : "title p2 color5"}>Item Name</div>
                 {items.map((item,i)=><div key={`$item.name}-${i}`} className={theme ? "p1 black":"p1 white"}>{item.name}</div>)}
@@ -32,6 +39,22 @@ const ItemsList = ({items}:ItemsListInterface):JSX.Element => {
                 {items.map((item,i)=><div key={`$item.name}-${i}`} className={theme ? "p1 black":"p1 white"}>€ {(item.price*item.quantity).toFixed(2)}</div>)}
             </div>
         </div>
+        {/* smartphone */}
+        <div className={theme ? "details smartphone colorBg13" : "details smartphone colorBg4"}>
+            {items.map(item=>
+                <div className="itemsLine">
+                    <div className="left">
+                        <p className={theme ? "p1 black":"p1 white"}>{item.name}</p>
+                        <p className={theme ? "p1 color7":"p1 color5"}>{item.quantity} x € {item.price.toFixed(2)}</p>
+                    </div>
+                    <div className="right">
+                        <p className={theme ? "p1 black":"p1 white"}>€ {(item.price*item.quantity).toFixed(2)}</p>
+                    </div>
+                </div>    
+            )}
+            
+        </div>
+        {/* sum */}
         <div className={theme ? "total colorBg14" : "total colorBg8"}>
             <p className="p2">Amount Due</p>
             <p className="totalNumber">€ {items.reduce((accu,current)=>{
